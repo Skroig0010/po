513cf45458bc217348c945faa19af74b1d68ed0c
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-06 15:03:07 +0900

    change files

--- my_context_ex_sample2/lib/my_context_ex_sample2/sensor/smoke_senror.ex
+++ my_context_ex_sample2/lib/my_context_ex_sample2/sensor/smoke_senror.ex
@@ -3,12 +3,16 @@ defmodule MyContextExSample2.Sensor.SmokeSensor do
     loop(pid)
   end
   def loop(pid) do
-    IO.inspect "smoke0";
-    raw = Python.call(:get_accelerometer_raw, [])
-    
-    send pid, %Event{type: :smoke, value: hd(raw) > 40}
-    :timer.sleep(1000)
-    IO.inspect "smoke1";
+    try do
+      IO.inspect "smoke0";
+      raw = Python.call(:get_accelerometer_raw, [])
+      
+      send pid, %Event{type: :smoke, value: hd(raw) > 40}
+      :timer.sleep(1000)
+      IO.inspect "smoke1";
+    rescue
+      e -> IO.puts "error: #{inspect e}"
+    end
     loop(pid)
   end
 end
