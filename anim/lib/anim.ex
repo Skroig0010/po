6462c37232f3e7404e01daf8a4090ed8bd048f9e
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-06 22:24:21 +0900

    change files

--- anim/lib/anim.ex
+++ anim/lib/anim.ex
@@ -20,7 +20,7 @@ defmodule Anim do
   def loop(rad) do
     cells = Enum.flat_map(0..7, fn y ->
       Enum.map(0..7, fn x ->
-        lumi = (:math.sin(rad + x / 4.0) + 1.0) / 2.0
+        lumi = (:math.sin(rad + x / 4.0) + 1.0) / 2.0 * (y + 1) / 8.0
         [trunc((:math.sin(rad) + 1.0) * 128 * lumi), trunc((:math.sin(rad + 1) + 1.0) * 128 * lumi), trunc((:math.sin(rad + 2) + 1.0) * 128)]
       end)
     end)
