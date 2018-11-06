90cfa13a70881f405cd8cf6d5e9d10619165e05f
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-06 22:30:41 +0900

    change files

--- anim/lib/anim.ex
+++ anim/lib/anim.ex
@@ -20,8 +20,11 @@ defmodule Anim do
   def loop(rad) do
     cells = Enum.flat_map(0..7, fn y ->
       Enum.map(0..7, fn x ->
-        lumi = (:math.sin(rad + x / 4.0) + 1.0) / 2.0 * (y + 1) / 8.0
-        [trunc((:math.sin(rad) + 1.0) * 128 * lumi), trunc((:math.sin(rad + 1) + 1.0) * 128 * lumi), trunc((:math.sin(rad + 2) + 1.0) * 128)]
+        case (:math.sin(rad + x / 4) + 1.0) * 4 do
+          a when a < y -> [trunc((:math.sin(rad) + 1.0) * 128), trunc((:math.sin(rad + 1) + 1.0) * 128), trunc((:math.sin(rad + 2) + 1.0) * 128)]
+          a when a == y -> [trunc((:math.sin(rad) + 1.0) * 64), trunc((:math.sin(rad + 1) + 1.0) * 64), trunc((:math.sin(rad + 2) + 1.0) * 64)]
+          a when a > y -> [0, 0, 0]
+        end
       end)
     end)
     Python.call(:"sense.set_pixels", [cells])
