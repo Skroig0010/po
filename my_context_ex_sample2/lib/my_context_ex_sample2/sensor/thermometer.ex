340f30ab284df7e39ed3781c0fa1c5f6d54bdff3
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-06 15:09:34 +0900

    change files

--- my_context_ex_sample2/lib/my_context_ex_sample2/sensor/thermometer.ex
+++ my_context_ex_sample2/lib/my_context_ex_sample2/sensor/thermometer.ex
@@ -3,11 +3,15 @@ defmodule MyContextExSample2.Sensor.Thermometer do
     loop(pid)
   end
   def loop(pid) do
-    IO.inspect "thermo0";
-    t = Python.call(:"sense.get_temperature", [])
-    send pid, %Event{type: :thermometer, value: t}
-    :timer.sleep(1000)
-    IO.inspect "thermo1";
+    try do
+      IO.inspect "thermo0";
+      t = Python.call(:"sense.get_temperature", [])
+      send pid, %Event{type: :thermometer, value: t}
+      :timer.sleep(1000)
+      IO.inspect "thermo1";
+    catch
+      _, e -> IO.puts "error: #{inspect e}"
+    end
     loop(pid)
   end
 end
