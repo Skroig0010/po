11024d1bda8f9670aade6cb28fc93ac4426d7f78
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-06 22:43:41 +0900

    change files

--- anim/lib/anim.ex
+++ anim/lib/anim.ex
@@ -20,7 +20,7 @@ defmodule Anim do
   def loop(rad) do
     cells = Enum.flat_map(0..7, fn y ->
       Enum.map(0..7, fn x ->
-        case trunc(qubic(rem(trunc(rad * 10) + x, 10) / 10) do
+        case trunc(qubic(rem(trunc(rad * 10) + x, 10) / 10)) do
           a when a <= y -> [trunc((:math.sin(rad) + 1.0) * 128), trunc((:math.sin(rad + 1) + 1.0) * 128), trunc((:math.sin(rad + 2) + 1.0) * 128)]
           a when a > y -> [0, 0, 0]
         end
