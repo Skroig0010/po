2ad7bf907723dc71bbbbd88d25416020b82e3f04
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-06 15:16:28 +0900

    change files

--- my_context_ex_sample2/lib/my_context_ex_sample2/actor.ex
+++ my_context_ex_sample2/lib/my_context_ex_sample2/actor.ex
@@ -34,25 +34,30 @@ defmodule MyContextExSample2.Actor do
   end
 
   defp receive_msg(%Event{type: :thermometer, value: val}) when val > 40 do
+    IO.inspect 0
     Python.call(:"sense.set_pixel", [0, 0, val, 0, 255])
     cast_activate_layer(%{:temperature => :high}) 
   end
 
   defp receive_msg(%Event{type: :thermometer, value: val}) when val <= 40 do 
+    IO.inspect 2
     Python.call(:"sense.set_pixel", [0, 0, val, 255, 255])
     cast_activate_layer(%{:temperature => :normal})
   end
 
   defp receive_msg(%Event{type: :smoke, value: val}) when val == true do
+    IO.inspect 3
     Python.call(:"sense.set_pixel", [0, 7, 255, 0, 255])
     cast_activate_layer(%{:smoke => true}) 
   end
 
   defp receive_msg(%Event{type: :smoke, value: val}) when val == false do 
+    IO.inspect 4
     Python.call(:"sense.set_pixel", [0, 7, 0, 255, 255])
     cast_activate_layer(%{:smoke => false})
   end
 
   defp receive_msg(_msg) do
+    IO.inspect 5
   end
 end
