9015e7f5ea588fb1b4e5a018b6603be1f1a87412
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-06 22:40:46 +0900

    change files

--- anim/lib/anim.ex
+++ anim/lib/anim.ex
@@ -20,9 +20,8 @@ defmodule Anim do
   def loop(rad) do
     cells = Enum.flat_map(0..7, fn y ->
       Enum.map(0..7, fn x ->
-        case trunc((:math.sin(rad + x / 10.0) + 1.0) * 4) do
-          a when a < y -> [trunc((:math.sin(rad) + 1.0) * 128), trunc((:math.sin(rad + 1) + 1.0) * 128), trunc((:math.sin(rad + 2) + 1.0) * 128)]
-          a when a == y -> [trunc((:math.sin(rad) + 1.0) * 64), trunc((:math.sin(rad + 1) + 1.0) * 64), trunc((:math.sin(rad + 2) + 1.0) * 64)]
+        case trunc(qubic(rem(rad + x / 4, 3) / 3)) do
+          a when a <= y -> [trunc((:math.sin(rad) + 1.0) * 128), trunc((:math.sin(rad + 1) + 1.0) * 128), trunc((:math.sin(rad + 2) + 1.0) * 128)]
           a when a > y -> [0, 0, 0]
         end
       end)
@@ -30,4 +29,12 @@ defmodule Anim do
     Python.call(:"sense.set_pixels", [cells])
     loop(rad + 0.01)
   end
+
+  def qubic(x) do
+    if x < 1 do
+      x * x * x
+    else 
+      x = x - 2
+      x * x * x + 2
+    end
 end
