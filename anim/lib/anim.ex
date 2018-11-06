e89f62293e54d507dccc2943a461a9f72a7980ec
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-06 22:00:28 +0900

    change files

--- anim/lib/anim.ex
+++ anim/lib/anim.ex
@@ -14,19 +14,20 @@ defmodule Anim do
   """
   def hello do
     Python.init()
-    loop(0.0, 0)
+    loop(0.0)
   end
 
-  def loop(rad, color) do
+  def loop(rad) do
     cells = Enum.flat_map(0..8, fn y ->
       Enum.flat_map(0..8, fn x ->
         if(y < (:math.sin(rad) + 1.0) * 4) do
-          [color, 255 - color, color + 128]
+          [trunc((:math.sin(rad) + 1.0) * 128), trunc((:math.sin(rad + 1) + 1.0) * 128), trunc((:math.sin(rad + 2) + 1.0) * 128)]
         else
           [0, 0, 0]
         end
       end)
     end)
     Python.call(:"sense.set_pixels", cells)
+    loop(rad + 0.01)
   end
 end
