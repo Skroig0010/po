c98d41bdec0f023a7fc3e5596441b9be2187d6ef
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-12 16:34:42 +0900

    いろいろ修正

--- router/lib/router.ex
+++ router/lib/router.ex
@@ -1,7 +1,7 @@
 defmodule Router do
   @spec route(integer, module, atom, [any()]) :: pid
   def route(n, module, fun, args) do
-    with node_name = String.to_atom("a@192.168.2." <> Integer.to_string(n + 160))
+    with node_name = String.to_atom("a@192.168.120." <> Integer.to_string(n + 160))
     do
       Node.spawn_link(node_name, module, fun, args)
     end
