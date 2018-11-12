846f745e472e13ecc14525d84340a869df707432
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-12 16:30:56 +0900

    いろいろ修正

--- my_context_ex_sample2/lib/my_context_ex_sample2.ex
+++ my_context_ex_sample2/lib/my_context_ex_sample2.ex
@@ -2,6 +2,7 @@ defmodule MyContextExSample2 do
   def start() do
     Python.init()
     actors = for n <- [0, 1, 3] do
+      IO.inspect n
       actor_pid = Router.route(n, MyContextExSample2.Actor, :start, [])
       Router.route(n, MyContextExSample2.Sensor.SmokeSensor, :start, [actor_pid])
       Router.route(n, MyContextExSample2.Sensor.Thermometer, :start, [actor_pid])
