942b00ca555dcbc8d18367bf7f8ff7ecf93dd054
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-22 14:41:42 +0900

    いろいろ修正

--- ping_pong/lib/ping_pong.ex
+++ ping_pong/lib/ping_pong.ex
@@ -3,7 +3,7 @@ defmodule PingPong do
   def start() do
     ContextEX.start()
     Python.init()
-    for n <- [0, 1] do
+    for n <- [1] do
       IO.inspect n
       actor_pid = Router.route(n, PingPong.Actor, :start, [])
       Router.route(n, PingPong.Sensor.Controller, :start, [actor_pid])
