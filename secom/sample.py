49a13cba22e3ac34642abc307cd2c5b74256db11
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-24 17:18:57 +0900

    pythonが間違ってた

--- secom/sample.py
+++ secom/sample.py
@@ -7,9 +7,9 @@ def get_accelerometer_raw():
     return [raw["x"], raw["y"], raw["z"]]
 
 def wait_for_event():
-    event = sense.wait_for_event()
+    event = sense.stick.wait_for_event()
     return [event.action, event.direction]
 
 def get_events():
-    events = sense.get_events()
+    events = sense.stick.get_events()
     return map(lambda event:[event.action, event.direction], events)
