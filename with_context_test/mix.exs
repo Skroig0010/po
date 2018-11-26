03c500bb9e21586a681cd97766143207aa773c09
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-26 18:16:08 +0900

    aiueo

--- with_context_test/mix.exs
+++ with_context_test/mix.exs
@@ -5,7 +5,7 @@ defmodule WithContextTest.MixProject do
     [
       app: :with_context_test,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
