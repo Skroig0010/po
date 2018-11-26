c25ad8b90c1b39a96d453e61f50db3f13c027fcd
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-26 16:24:11 +0900

    人感センサーの情報を受け取れるようにした

--- secom/lib/secom/actor.ex
+++ secom/lib/secom/actor.ex
@@ -33,31 +33,41 @@ defmodule Secom.Actor do
     loop()
   end
 
-  defp receive_msg(%Secom.Event{type: :thermometer, value: val}) when val > 40 do
+  deflfp receive_msg(%Secom.Event{type: :thermometer, value: val}), %{} when val > 40 do
     Python.call(:"sense.set_pixel", [0, 0, rem(trunc(val * 100), 255), 0, 255])
     cast_activate_layer(%{:temperature => :high}) 
   end
 
-  defp receive_msg(%Secom.Event{type: :thermometer, value: val}) when val <= 40 do 
+  deflfp receive_msg(%Secom.Event{type: :thermometer, value: val}), %{} when val <= 40 do 
     Python.call(:"sense.set_pixel", [0, 0, rem(trunc(val * 100), 255), 255, 255])
     cast_activate_layer(%{:temperature => :normal})
   end
 
-  defp receive_msg(%Secom.Event{type: :smoke, value: val}) when val == true do
+  deflfp receive_msg(%Secom.Event{type: :smoke, value: val}), %{} when val == true do
     Python.call(:"sense.set_pixel", [0, 7, 255, 0, 255])
     cast_activate_layer(%{:smoke => true}) 
   end
 
-  defp receive_msg(%Secom.Event{type: :smoke, value: val}) when val == false do 
+  deflfp receive_msg(%Secom.Event{type: :smoke, value: val}), %{} when val == false do 
     Python.call(:"sense.set_pixel", [0, 7, 0, 255, 255])
     cast_activate_layer(%{:smoke => false})
   end
 
-  defp receive_msg(%Secom.Event{type: :updater, value: 0}) do 
+  deflfp receive_msg(%Secom.Event{type: :human, value: val}), %{:time => time} when val == true and (time > 11 or time < 5) do
+    Python.call(:"sense.set_pixel", [0, 3, 255, 255, 255])
+    cast_activate_layer(%{:suspicious_person => true})
+  end
+
+  deflfp receive_msg(%Secom.Event{type: :human, value: _}) do
+    Python.call(:"sense.set_pixel", [0, 3, 0, 0, 0])
+    cast_activate_layer(%{:suspicious_person => false})
+  end
+
+  deflfp receive_msg(%Secom.Event{type: :updater, value: 0}) do 
     Python.call(:"sense.set_pixel", [3, 3, 255, 255, 255])
   end
 
-  defp receive_msg(msg) do
+  deflfp receive_msg(msg) do
     IO.puts "there is no function for this↓ message."
     IO.inspect msg
   end
