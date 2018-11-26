821d1532ac6413dd09ff3597d0b1b5ae92af0a16
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-26 18:16:08 +0900

    aiueo

--- secom/lib/secom/actor.ex
+++ secom/lib/secom/actor.ex
@@ -33,6 +33,7 @@ defmodule Secom.Actor do
     loop()
   end
 
+  # thermometer
   deflfp receive_msg(%Secom.Event{type: :thermometer, value: val}), %{} when val > 40 do
     Python.call(:"sense.set_pixel", [0, 0, rem(trunc(val * 100), 255), 0, 255])
     cast_activate_layer(%{:temperature => :high}) 
@@ -43,6 +44,7 @@ defmodule Secom.Actor do
     cast_activate_layer(%{:temperature => :normal})
   end
 
+  # smoke sensor
   deflfp receive_msg(%Secom.Event{type: :smoke, value: val}), %{} when val == true do
     Python.call(:"sense.set_pixel", [0, 7, 255, 0, 255])
     cast_activate_layer(%{:smoke => true}) 
@@ -53,6 +55,7 @@ defmodule Secom.Actor do
     cast_activate_layer(%{:smoke => false})
   end
 
+  # human sensor
   deflfp receive_msg(%Secom.Event{type: :human, value: val}), %{:time => time} when val == true and (time > 11 or time < 5) do
     Python.call(:"sense.set_pixel", [0, 3, 255, 255, 255])
     cast_activate_layer(%{:suspicious_person => true})
@@ -63,10 +66,12 @@ defmodule Secom.Actor do
     cast_activate_layer(%{:suspicious_person => false})
   end
 
+  # updater
   deflfp receive_msg(%Secom.Event{type: :updater, value: 0}) do 
     Python.call(:"sense.set_pixel", [3, 3, 255, 255, 255])
   end
 
+  # default method
   deflfp receive_msg(msg) do
     IO.puts "there is no function for the following message."
     IO.inspect msg
