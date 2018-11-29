8ca1fb5b5cc5c2de67baf8cfdb986049a049683c
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-29 17:55:01 +0900

    修正

--- secom/lib/secom/actor.ex
+++ secom/lib/secom/actor.ex
@@ -14,6 +14,10 @@ defmodule Secom.Actor do
     loop()
   end
 
+  deflf loop(), %{:suspicious_person => true} do
+    Python.call(:"sense.show_message", ["reporting suspicious person"])
+  end
+
   deflf loop(), %{:temperature => :high, :smoke => true} do
     Python.call(:"sense.show_message", ["fire"])
     cast_activate_group(:actor, %{:status => :emergency})
@@ -34,29 +38,29 @@ defmodule Secom.Actor do
   end
 
   # thermometer
-  deflfp receive_msg(%Secom.Event{type: :thermometer, value: val}), %{} when val > 40 do
+  deflfp receive_msg(%Secom.Event{type: :thermometer, value: val}) when val > 40 do
     Python.call(:"sense.set_pixel", [0, 0, rem(trunc(val * 100), 255), 0, 255])
     cast_activate_layer(%{:temperature => :high}) 
   end
 
-  deflfp receive_msg(%Secom.Event{type: :thermometer, value: val}), %{} when val <= 40 do 
+  deflfp receive_msg(%Secom.Event{type: :thermometer, value: val}) when val <= 40 do 
     Python.call(:"sense.set_pixel", [0, 0, rem(trunc(val * 100), 255), 255, 255])
     cast_activate_layer(%{:temperature => :normal})
   end
 
   # smoke sensor
-  deflfp receive_msg(%Secom.Event{type: :smoke, value: val}), %{} when val == true do
+  deflfp receive_msg(%Secom.Event{type: :smoke, value: val}) when val == true do
     Python.call(:"sense.set_pixel", [0, 7, 255, 0, 255])
     cast_activate_layer(%{:smoke => true}) 
   end
 
-  deflfp receive_msg(%Secom.Event{type: :smoke, value: val}), %{} when val == false do 
+  deflfp receive_msg(%Secom.Event{type: :smoke, value: val}) when val == false do 
     Python.call(:"sense.set_pixel", [0, 7, 0, 255, 255])
     cast_activate_layer(%{:smoke => false})
   end
 
   # human sensor
-  deflfp receive_msg(%Secom.Event{type: :human, value: val}), %{:time => time} when val == true and (time > 11 or time < 5) do
+  deflfp receive_msg(%Secom.Event{type: :human, value: val}), %{:time => time} when val == true and (time > 23 or time < 5) do
     Python.call(:"sense.set_pixel", [0, 3, 255, 255, 255])
     cast_activate_layer(%{:suspicious_person => true})
   end
