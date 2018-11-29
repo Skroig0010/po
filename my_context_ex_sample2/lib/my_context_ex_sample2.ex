93ad18d580d239bb9f12b91ad23bc9194d22b8cb
Tsukasa Gotoh <go10.2ka3@ezweb.ne.jp>
2018-11-29 18:22:12 +0900

    あれ

--- my_context_ex_sample2/lib/my_context_ex_sample2.ex
+++ my_context_ex_sample2/lib/my_context_ex_sample2.ex
@@ -2,7 +2,8 @@ defmodule MyContextExSample2 do
   def start() do
     ContextEX.start()
     Python.init()
-    actors = for n <- [0, 1, 3] do
+    Router.connect_all([0])
+    actors = for n <- [0] do
       IO.inspect n
       actor_pid = Router.route(n, MyContextExSample2.Actor, :start, [])
       Router.route(n, MyContextExSample2.Sensor.SmokeSensor, :start, [actor_pid])
