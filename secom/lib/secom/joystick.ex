dfa927117cd61f7a7397a5de0acb4527d8bf0667
Skroig0010 <go10.2ka3@ezweb.ne.jp>
2019-01-05 16:09:15 +0900

    actionとdirectionの順序ミス

--- secom/lib/secom/joystick.ex
+++ secom/lib/secom/joystick.ex
@@ -11,11 +11,11 @@ defmodule Secom.Joystick do
   def update() do
     event_list = Python.call(:get_events, [])
 
-    left = Enum.any?(event_list, fn {_, dir} -> dir == 'left' end)
-    right = Enum.any?(event_list, fn {_, dir} -> dir == 'right' end)
-    up = Enum.any?(event_list, fn {_, dir} -> dir == 'up' end)
-    down = Enum.any?(event_list, fn {_, dir} -> dir == 'down' end)
-    action = if(Enum.any?(event_list, fn {act, _} -> act == 'pressed' end)) do
+    left = Enum.any?(event_list, fn {dir, _} -> dir == 'left' end)
+    right = Enum.any?(event_list, fn {dir, _} -> dir == 'right' end)
+    up = Enum.any?(event_list, fn {dir, _} -> dir == 'up' end)
+    down = Enum.any?(event_list, fn {dir, _} -> dir == 'down' end)
+    action = if(Enum.any?(event_list, fn {_, act} -> act == 'pressed' end)) do
       :pressed
     else
       :released
@@ -26,12 +26,12 @@ defmodule Secom.Joystick do
 
   @spec get_direction() :: direction
   def get_direction() do 
-    Agent.get(Process.whereis(@joystick), fn {_, dir} -> dir end)
+    Agent.get(Process.whereis(@joystick), fn {dir, _} -> dir end)
   end
 
   @spec get_action() :: action
   def get_action() do
-    Agent.get(Process.whereis(@joystick), fn {act, _} -> act end)
+    Agent.get(Process.whereis(@joystick), fn {_, act} -> act end)
   end
 
 end
