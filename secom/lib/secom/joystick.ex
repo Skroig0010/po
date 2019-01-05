208c851516f458891d2c53423508b7d41001e5c3
Skroig0010 <go10.2ka3@ezweb.ne.jp>
2019-01-05 17:13:32 +0900

    functionの引数の型が間違っていたのを修正

--- secom/lib/secom/joystick.ex
+++ secom/lib/secom/joystick.ex
@@ -9,34 +9,36 @@ defmodule Secom.Joystick do
   end
 
   def update() do
-    try do
-      event_list = Python.call(:get_events, [])
+    event_list = Python.call(:get_events, [])
+    IO.inspect event_list
 
-      left = Enum.any?(event_list, fn {dir, _} -> dir == 'left' end)
-      right = Enum.any?(event_list, fn {dir, _} -> dir == 'right' end)
-      up = Enum.any?(event_list, fn {dir, _} -> dir == 'up' end)
-      down = Enum.any?(event_list, fn {dir, _} -> dir == 'down' end)
-      action = if(Enum.any?(event_list, fn {_, act} -> act == 'pressed' end)) do
-        :pressed
-      else
-        :released
-      end
+    left = Enum.any?(event_list, fn [dir, _] -> dir == 'left' end)
+    right = Enum.any?(event_list, fn [dir, _] -> dir == 'right' end)
+    up = Enum.any?(event_list, fn [dir, _] -> dir == 'up' end)
+    down = Enum.any?(event_list, fn [dir, _] -> dir == 'down' end)
+    action = if(Enum.any?(event_list, fn [_, act] -> act == 'pressed' end)) do
+      :pressed
+    else
+      :released
+    end
 
+    if(Process.whereis(@joystick) != nil) do
       Agent.update(Process.whereis(@joystick), fn _ -> {{up, down, left, right}, action} end)
-    catch
-      x, e -> IO.puts "error on joystick: #{inspect e} : #{inspect x}"
-        IO.puts "joystick pid is #{Process.whereis(@joystick)}}"
     end
   end
 
   @spec get_direction() :: direction
   def get_direction() do 
-    Agent.get(Process.whereis(@joystick), fn {dir, _} -> dir end)
+    if(Process.whereis(@joystick) != nil) do
+      Agent.get(Process.whereis(@joystick), fn {dir, _} -> dir end)
+    end
   end
 
   @spec get_action() :: action
   def get_action() do
-    Agent.get(Process.whereis(@joystick), fn {_, act} -> act end)
+    if(Process.whereis(@joystick) != nil) do
+      Agent.get(Process.whereis(@joystick), fn {_, act} -> act end)
+    end
   end
 
 end
