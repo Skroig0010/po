bc6110c29adb6168474e0c94c36f64eae791e0c7
Skroig0010 <go10.2ka3@ezweb.ne.jp>
2019-01-07 15:57:38 +0900

    窓開けテスト

--- green_house/lib/green_house/date.ex
+++ green_house/lib/green_house/date.ex
@@ -3,14 +3,16 @@ defmodule GreenHouse.Date do
 
   def start() do
     IO.puts "start clock"
-    init_context(:timer)
+    init_context(:date)
     loop()
   end
 
   deflfp loop() do
     date = Date.utc_today()
-    cast_activate_group(:actor, %{:month => date.month})
-    cast_activate_group(:sink, %{:month => date.month})
+    cast_activate_group(:actor, %{:month => 8})
+    cast_activate_group(:sink, %{:month => 8})
+    # cast_activate_group(:actor, %{:month => date.month})
+    # cast_activate_group(:sink, %{:month => date.month})
     :timer.sleep(10000)
     loop()
   end
