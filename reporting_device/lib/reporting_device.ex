d43b54585dc55b76c23f6f0cde2c0cb3a4377e46
Skroig0010 <go10.2ka3@ezweb.ne.jp>
2019-01-14 14:15:27 +0900

    macでsensehat使えないので修正

--- reporting_device/lib/reporting_device.ex
+++ reporting_device/lib/reporting_device.ex
@@ -5,7 +5,6 @@ defmodule ReportingDevice do
   @spec start() :: pid
   def start() do
     ContextEX.start()
-    Python.init()
     Router.connect_all(@actors)
     for id <- @actors do
       IO.inspect id
@@ -13,7 +12,6 @@ defmodule ReportingDevice do
       Router.route(id, ReportingDevice.Actuator.Display, :start, [])
       Router.route(id, ReportingDevice.Actor, :start, [false])
     end
-    # sinkで起動したい
     pid = spawn_link(ReportingDevice.Actor, :start, [true])
     spawn_link(ReportingDevice.Sensor.HumanSensor, :start, [pid])
     spawn_link(ReportingDevice.Clock, :start, [])
--- reporting_device/lib/reporting_device/actor.ex
+++ reporting_device/lib/reporting_device/actor.ex
@@ -30,20 +30,11 @@ defmodule ReportingDevice.Actor do
 
   # human sensor
   deflfp receive_msg(%ReportingDevice.Event{type: :human, value: val}), %{:time => time} when val == true and (time > 23 or time < 5) do
-    Python.call(:"sense.set_pixel", [0, 3, 255, 255, 255])
     cast_activate_layer(%{:suspicious_person => true})
   end
 
   deflfp receive_msg(%ReportingDevice.Event{type: :human, value: _}) do
-    Python.call(:"sense.set_pixel", [0, 3, 0, 0, 0])
     cast_activate_layer(%{:suspicious_person => false})
-  end
-
-  # updater
-  # センサーをjoystickで代用してるのでjoystickの入力がないとloop()が止まってしまい、コンテキスト変化に対応できない
-  # ちゃんとしたセンサーを乗せたら必要なくなる
-  deflfp receive_msg(%ReportingDevice.Event{type: :updater, value: _val}) do 
-    Python.call(:"sense.set_pixel", [3, 3, 255, 255, 255])
   end
 
   # default method
